b04b460254317bbddf69b3eb460a7d286fcf343a
Mohammad Ashif <mohammadashif510@gmail.com>
2025-07-12 23:21:40 +0600

    user module completed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
-import express, { Request, Response } from "express";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { router } from "./app/routes";
+import { globalErrorHandlers } from "./app/middlewars/globalErrorHandler";
+import httpStatus from "http-status-codes";
+import { apiNotFound } from "./app/middlewars/notFound";
 
 const app = express();
 
@@ -12,5 +16,9 @@ app.use("/api/v1", router);
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to Fairhaven");
 });
+
+app.use(globalErrorHandlers);
+
+app.use(apiNotFound);
 
 export default app;
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,26 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response } from "express";
-import { User } from "./user.model";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
-const createUser = async (req: Request, res: Response) => {
-    try {
-        const { name, email } = req.body;
-        const user = await User.create({ name, email });
-        res.status(httpStatus.CREATED).json({
+const createUser = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const user = await UserServices.createUserIntoDB(req.body);
+        sendResponse(res, {
+            statusCode: httpStatus.CREATED,
             success: true,
             message: "User created successfully",
-            user,
+            data: user,
         });
-    } catch (error: any) {
-        console.log(error);
-        res.status(httpStatus.BAD_REQUEST).json({
-            message: `Something went wrong ${error.message}`,
-        });
+        // res.status(httpStatus.CREATED).json({
+        //     message: "User created successfully",
+        //     user,
+        // });
     }
-};
+);
 
+const getAllUsers = catchAsync(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const result = await UserServices.getAllUsersFromDB();
+        sendResponse(res, {
+            statusCode: httpStatus.CREATED,
+            success: true,
+            message: "All users retrieve successfully",
+            data: result.data,
+            meta: result.meta,
+        });
+        // res.status(httpStatus.OK).json({
+        //     success: true,
+        //     message: "All user retrieve successfully",
+        //     users,
+        // });
+    }
+);
+
+// const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         const users = await UserServices.getAllUsersFromDB();
+//         res.status(httpStatus.OK).json({
+//             success: true,
+//             message: "All user retrieve successfully",
+//             users,
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// };
 
 export const UserControllers = {
-    createUser
-}
+    createUser,
+    getAllUsers,
+};
